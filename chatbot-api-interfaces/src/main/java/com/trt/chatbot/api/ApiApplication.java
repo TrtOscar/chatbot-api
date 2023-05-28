3f7bfe1186711e2883cb2d4663db848c7b9b10dc
谭韧泰 <3747641@qq.com>
2023-05-28 22:11:34 +0800

    TRT feat:init project

---
+++ chatbot-api-interfaces/src/main/java/com/trt/chatbot/api/ApiApplication.java
@@ -0,0 +1,14 @@
+package com.trt.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author Oscar
+ * @description 启动入口
+ * @github https://github.com/TrtOscar
+ */
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
